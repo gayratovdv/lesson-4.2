57cc4a9cb2aa444fa1dd39a323ff7a5b3936daab
javohirgvdev <gayratovjavohir90@gmail.com>
2024-03-12 11:47:15 +0500

    I added javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+let boolean = Boolean(prompt("Boolean typedagi malumotlarni kiriting")); 
+let number = Number(prompt("Number typedagi malumotlarni kiriting"));
+let text = prompt("String typedagi malumotlarni kiriting");
+alert("Hamma ma'lumotlarni to'g'ri kiritganingizga ishonchingiz komilmi?")
+
+alert("Siz kiritgan " + (boolean) + " so'zi boolean typega tegishli" + "\n" + "Siz kiritgan " + (number) + " so'zi number typega tegishli" + "\n" + "Siz kiritgan " + (text) + " so'zi string typega tegishli");
